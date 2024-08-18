9b0e1bcff96875ea2e08b5d2a9742f1a9d032f25
Lussuru <Lussuru17@mail.ru>
2024-08-18 21:24:32 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+const PHOTOS_COUNT = 25;
+const MIN_LIKES_COUNT = 15;
+const MAX_LIKES_COUNT = 200;
+const AVATAR_COUNT = 6;
+const COMMENTS_COUNT = 10;
+
 const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -17,52 +23,72 @@ const NAMES = [
   'Илья'
 ];
 const DESCRIPTIONS = [
-  'Черный черный кот',
-  'Закат на берегу моря',
-  'Вкусное шоколадное мороженко',
-  'Кусты сирени',
-  'Одинокое дерево'
+  'Золотой закат над горизонтом океана.',
+  'Уютная деревенская улица с цветущими садами.',
+  'Древний замок на вершине холма.',
+  'Легкий утренний туман над озером.',
+  'Яркий городской рынок в вечернее время.',
+  'Величественные горы, покрытые снегом.',
+  'Модный городской ландшафт с небоскребами.',
+  'Восход солнца на пляже с золотым песком.',
+  'Спокойное сельское поле с размытой линией горизонта.',
+  'Творческое уличное искусство на стенах зданий.',
+  'Густой лес в осенний период с яркими листьями.',
+  'Очаровательная старинная библиотека с деревянными полками.',
+  'Современное кафе с стильным интерьером.',
+  'Счастливые дети, играющие в парке.',
+  'Элегантный балетный спектакль на сцене.',
+  'Прекрасный вид на мост, соединяющий два берега реки.',
+  'Динамичные уличные сцены на ночном фестивале.',
+  'Старинный маяк на краю утеса.',
+  'Красивый зимний пейзаж с замороженным озером.',
+  'Тихий внутренний дворик с фонтаном.',
+  'Корабли в порту на фоне заката.',
+  'Романтичный вечерний ужин на террасе с видом на море.',
+  'Живописный пейзаж с лавандовыми полями.',
+  'Модные обувные витрины в центре города.',
+  'Величественные архитектурные детали исторического здания.'
 ];
-const MAX_PHOTOS_COUNT = 25;
-const MIN_COMMENT_ID = 1;
-const MAX_COMMENT_ID = 200;
-const MIN_AVATAR_NUMBER = 1;
-const MAX_AVATAR_NUMBER = 6;
-const MIN_PHOTO_ID = 1;
-const MAX_PHOTO_ID = 25;
-const MIN_PHOTO_NUMBER = 1;
-const MAX_PHOTO_NUMBER = 25;
-const MIN_LIKES_COUNT = 15;
-const MAX_LIKES_COUNT = 200;
-const MIN_COMMENTS_COUNT = 1;
-const MAX_COMMENTS_COUNT = 6;
 
 const getRandomInteger = (min, max) => {
-  const minInteger = Math.ceil(Math.min(min, max));
-  const maxInteger = Math.floor(Math.max(min, max));
-  const result = Math.random() * (maxInteger - minInteger + 1) + minInteger;
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+  const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 };
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-const generateComments = () => ({
-  id: getRandomInteger(MIN_COMMENT_ID, MAX_COMMENT_ID),
-  avatar: `img/avatar-${getRandomInteger(MIN_AVATAR_NUMBER, MAX_AVATAR_NUMBER)}.svg`,
-  message: getRandomArrayElement(MESSAGES),
+const createIndexGenerator = () => {
+  let lastGeneratedId = 0;
+
+  return () => {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+};
+
+const createCommentId = createIndexGenerator();
+const createPhotoId = createIndexGenerator();
+const createPhotoPathId = createIndexGenerator();
+
+const createMessage = () => Array.from({ length: getRandomInteger(1, 2) }, () => getRandomArrayElement(MESSAGES)).join(' ');
+
+const createComments = () => ({
+  id: createCommentId(),
+  avatar: `img/avatar-${getRandomInteger(1, AVATAR_COUNT)}.svg`,
+  message: createMessage(),
   name: getRandomArrayElement(NAMES)
 });
 
 const createPhotos = () => ({
-  id: getRandomInteger(MIN_PHOTO_ID, MAX_PHOTO_ID),
-  url: `photos/${getRandomInteger(MIN_PHOTO_NUMBER, MAX_PHOTO_NUMBER)}.jpg`,
+  id: createPhotoId(),
+  url: `photos/${createPhotoPathId()}.jpg`,
   description: getRandomArrayElement(DESCRIPTIONS),
   likes: getRandomInteger(MIN_LIKES_COUNT, MAX_LIKES_COUNT),
-  comments: Array.from({length : getRandomInteger(MIN_COMMENTS_COUNT, MAX_COMMENTS_COUNT)}, generateComments)
+  comments: Array.from({length : getRandomInteger(1, COMMENTS_COUNT)}, createComments)
 });
 
-const getPhotos = () => Array.from({length: MAX_PHOTOS_COUNT}, createPhotos);
+const getPhotos = () => Array.from({length: PHOTOS_COUNT}, createPhotos);
 
 getPhotos();
-
-
